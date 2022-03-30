f623388075434de221960703d29a76539e3bf0ca
Bilas Denys <bilasdenyswork@gmail.com>
2022-03-30 13:15:04 +0300

    created routes structure for application

---
+++ src/Layouts/CategoriesPageComponent/CategoriesPageComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CategoriesPageComponent.scss';
+
+const CategoriesPageComponent: React.FC = (): JSX.Element => {
+  return (
+    <div>CategoriesPageComponent</div>
+  )
+}
+
+export { CategoriesPageComponent }
---
+++ src/Layouts/HomePageComponent/HomePageComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomePageComponent.scss';
+
+const HomePageComponent: React.FC = (): JSX.Element => {
+  return (
+    <section>HomePageComponent</section>
+  )
+}
+
+export { HomePageComponent };
